acf48978344c579e1cfe66ddfae86258a08bd74c
crifan <admin@crifan.com>
2021-01-16 09:13:07 +0800

    1. update book gitbook_demo

--- gitbook_demo/website/gitbook/gitbook-plugin-google-adsense/plugin.js
+++ gitbook_demo/website/gitbook/gitbook-plugin-google-adsense/plugin.js
@@ -1,55 +1,48 @@
 require(["gitbook"], function(gitbook) {
     // plugin config
-    var config;
+    // var config;
 
-    function createAdElement(adConfig) {
-        adElement = document.createElement('ins');
-        adElement.style = 'display: block';
-        adElement.className = 'adsbygoogle';
-        adElement.setAttribute('data-ad-client', adConfig.client);
-        adElement.setAttribute('data-ad-slot', adConfig.slot);
-        adConfig.format && adElement.setAttribute('data-ad-format', adConfig.format);
-        // adConfig.style && adElement.setAttribute('style', adConfig.style);
-        adElement.setAttribute('data-full-width-responsive', "true");
+    // function createAdElement(adConfig) {
+    //     adElement = document.createElement('ins');
+    //     adElement.style = 'display: block';
+    //     adElement.className = 'adsbygoogle';
+    //     adElement.setAttribute('data-ad-client', adConfig.client);
+    //     adElement.setAttribute('data-ad-slot', adConfig.slot);
+    //     adConfig.format && adElement.setAttribute('data-ad-format', adConfig.format);
+    //     adConfig.style && adElement.setAttribute('style', adConfig.style);
 
-        // console.log("adElement=%o", adElement);
-        return adElement;
-    }
+    //     return adElement;
+    // }
 
-    function injectAds(configs) {
-        if(configs && configs.length > 0) {
-            configs.forEach(function(c) {
-                document.querySelector(c.location).appendChild(createAdElement(c));
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            });
-
-            // configs.forEach(function(c) {
-            //     console.log("after injectAds, related html=%s", document.querySelector(c.location));
-            //     console.log(document.querySelector(c.location));
-            // });
-        }
-    }
+    // function injectAds(configs) {
+    //     if(configs && configs.length > 0) {
+    //         configs.forEach(function(c) {
+    //             document.querySelector(c.location).appendChild(createAdElement(c));
+    //             (adsbygoogle = window.adsbygoogle || []).push({});
+    //         });
+    //     }
+    // }
 
     gitbook.events.bind("start", function(e, pluginConfig) {
-        // console.log("=================== google-adsense start: pluginConfig=%s", pluginConfig);
-        config = pluginConfig['google-adsense'].ads;
+        console.log("=================== google-adsense start: pluginConfig=%o", pluginConfig);
+        configs = pluginConfig['google-adsense'].ads;
+        console.log("configs=%o", configs);
+        firstConfig = configs[0]
+        firstClient = firstConfig.client
+        console.log("firstClient=%o", firstClient);
 
-        // console.log("config=%o", config);
         // init script
         var adScript = document.createElement('script');
-        // adScript.src = '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
-        adScript.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+        adScript.src = '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
         adScript.setAttribute('async', true);
-        // console.log("adScript=%o", adScript);
+        adScript.setAttribute('data-ad-client', firstClient); // add for Google Adsense Auto Ads
+        console.log("adScript=%o", adScript);
         document.body.appendChild(adScript);
     });
 
-    gitbook.events.bind("page.change", function() {
-        // console.log("=================== google-adsense page.change");
-        // console.log("config=%o", config);
-        if (config) {
-            injectAds(config);
-            // console.log("injectAds done");
-        }
-    });
+    // gitbook.events.bind("page.change", function() {
+    //     if (config) {
+    //         injectAds(config);
+    //     }
+    // });
 });
